6fafb333c68f73b32ec37062797e35bc398c2b18
Cristian Romo <cristian.g.romo@gmail.com>
2016-12-09 21:50:40 -0600

    Add initial set of libraries

--- main.lua
+++ main.lua
@@ -1,3 +1,15 @@
+local sprites = require 'sprites'
+local assets = require 'assets'
+
+function love.load()
+  assets.register('png', sprites.Sheet.load)
+  assets.load('assets')
+end
+
+function love.update(dt)
+  require("lovebird").update()
+end
+
 function love.draw()
   love.graphics.print("Hello, LD37")
 end
