2d59678d8697b00c1a04a3499250b6b76d322b24
Cristian Romo <cristian.g.romo@gmail.com>
2016-12-10 00:04:45 -0600

    Add PoC pinch shader

--- assets/pinch.frag
+++ assets/pinch.frag
@@ -2,5 +2,8 @@ uniform vec2 center;
 uniform float radius;
 
 vec4 effect(vec4 color, sampler2D texture, vec2 uv, vec2 screen) {
-  return vec4(1, 0, 1, 1);
+  if (distance(center, uv) < radius) {
+    return texture2D(texture, uv);
+  }
+  return vec4(1, 0, 0, 0);
 }
--- main.lua
+++ main.lua
@@ -1,9 +1,17 @@
 local sprites = require 'sprites'
 local assets = require 'assets'
 
+local state = {}
+
+local tiles
 function love.load()
   assets.register('png', sprites.Sheet.load)
+  assets.register('frag', love.graphics.newShader)
   assets.load('assets')
+
+  tiles = sprites.Sprite.new(assets.starting_tiles)
+  state.red = love.graphics.newCanvas(100, 100)
+  state.blue = love.graphics.newCanvas(100, 100)
 end
 
 function love.update(dt)
@@ -11,5 +19,23 @@ function love.update(dt)
 end
 
 function love.draw()
+  love.graphics.setColor(255, 255, 255)
+  love.graphics.setShader()
   love.graphics.print("Hello, LD37")
+  tiles:draw()
+
+  -- love.graphics.rectangle('fill', 20, 20, 100, 100)
+  love.graphics.setColor(255, 0, 0)
+  state.red:renderTo(function() love.graphics.rectangle('fill', 0, 0, 100, 100) end)
+  -- love.graphics.rectangle('fill', 30, 30, 100, 100)
+  love.graphics.setColor(0, 0, 255)
+  state.blue:renderTo(function() love.graphics.rectangle('fill', 0, 0, 100, 100) end)
+
+  love.graphics.setColor(255, 255, 255)
+  love.graphics.draw(state.red, 20, 20)
+  local pinch = assets.pinch
+  love.graphics.setShader(pinch)
+  pinch:send('center', {0.5, 0.5})
+  pinch:send('radius', 0.3)
+  love.graphics.draw(state.blue, 30, 30)
 end
